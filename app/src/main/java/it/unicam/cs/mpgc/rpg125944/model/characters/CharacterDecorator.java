65c7e94c9160be52e9084833d69ee8771284f1be
hasansanii03 <mu353358@gmail.com>
2026-07-11 15:16:12 +0200

    completata la base del model

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg125944/model/characters/CharacterDecorator.java
@@ -0,0 +1,50 @@
+package it.unicam.cs.mpgc.rpg125944.model.characters; 
+
+/**
+ * Classe astratta base per il Decorator Pattern.
+ * Implementa Character e contiene un riferimento al Character da decorare.
+ */
+public abstract class CharacterDecorator implements Character {
+    
+    // Protected per permettere alle sottoclassi (le armi/armature) di accedervi
+    protected Character character;
+
+    public CharacterDecorator(Character character) {
+        this.character = character;
+    }
+
+    @Override
+    public String getName() {
+        return character.getName();
+    }
+
+    @Override
+    public int getHp() {
+        return character.getHp();
+    }
+
+    @Override
+    public int getMaxHp() {
+        return character.getMaxHp();
+    }
+
+    @Override
+    public int getAttackPower() {
+        return character.getAttackPower();
+    }
+
+    @Override
+    public int getDefense() {
+        return character.getDefense();
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        character.takeDamage(damage);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return character.isAlive();
+    }
+}
